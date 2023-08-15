d7f0b01cf12709da1ff44051a92c097c7358c103
Bui Huu Dat <dat54261001@gmail.com>
2023-08-15 10:03:15 +0700

    push

--- src/pages/Cart/components/PayInformation.tsx
+++ src/pages/Cart/components/PayInformation.tsx
@@ -64,15 +64,15 @@ const PayInformation = memo(() => {
 
   useEffect(() => {
     setAddress(
-      user.address.more +
+      user.address?.more +
         " " +
-        user.address.street +
+        user.address?.street +
         " " +
-        user.address.ward +
+        user.address?.ward +
         " " +
-        user.address.district +
+        user.address?.district +
         " " +
-        user.address.city
+        user.address?.city
     );
   }, [user.address]);
 
